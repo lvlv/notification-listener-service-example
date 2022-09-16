de1bfdf763733f5b5ea0f9ab74925db5c00aa320
lvlv <github@lvlv.email>
2022-09-17 00:38:09 +0800

    init notification parser

---
+++ app/src/main/java/com/github/chagall/notificationlistenerexample/BgValue.java
@@ -0,0 +1,65 @@
+package com.github.chagall.notificationlistenerexample;
+
+import android.util.Log;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class BgValue {
+    public double bgValue;
+    public Timestamp timestamp;
+    public String direction;
+    public String delta;
+
+    private static String TAG = "NOTI_LISTENER";
+
+    public void setBgValue(String value, String unit) {
+        try {
+            if (unit.equals("mmol")) {
+                bgValue = Double.parseDouble(value) * 18.0;
+            } else if (unit == "mgdl") {
+                bgValue = Double.parseDouble(value);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "setBgValue failed: " + value + " " + unit);
+        }
+    }
+
+    private static DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private static DateFormat day = new SimpleDateFormat("yyyy-MM-dd");
+    private static DateFormat ts = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public void setTimestamp(String hhmm) {
+        long now = System.currentTimeMillis();
+        String today = day.format(new Timestamp((now)));
+        try {
+            timestamp = Timestamp.valueOf(ts.format(formatter.parse(today + " " + hhmm)));
+        } catch (Exception e) {
+            Log.e(TAG, "Error parse " + hhmm);
+            timestamp = new Timestamp(now);
+        }
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public void setDelta(String delta) {
+        this.delta = delta;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("bg: ");
+        builder.append(bgValue);
+        builder.append(" ts: ");
+        builder.append(timestamp);
+        builder.append(" direction: ");
+        builder.append(direction);
+        builder.append(" delta: ");
+        builder.append(delta);
+        return builder.toString();
+    }
+}
--- app/src/main/java/com/github/chagall/notificationlistenerexample/NotificationListenerExampleService.java
+++ app/src/main/java/com/github/chagall/notificationlistenerexample/NotificationListenerExampleService.java
@@ -1,9 +1,23 @@
 package com.github.chagall.notificationlistenerexample;
 
+import android.app.Notification;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * MIT License
@@ -26,6 +40,8 @@ import android.service.notification.StatusBarNotification;
  */
 public class NotificationListenerExampleService extends NotificationListenerService {
 
+    private static String TAG = "NOTI_LISTENER";
+
     /*
         These are the package names of the apps. for which we want to
         listen the notifications
@@ -35,6 +51,8 @@ public class NotificationListenerExampleService extends NotificationListenerServ
         public static final String FACEBOOK_MESSENGER_PACK_NAME = "com.facebook.orca";
         public static final String WHATSAPP_PACK_NAME = "com.whatsapp";
         public static final String INSTAGRAM_PACK_NAME = "com.instagram.android";
+        public static final String CGMCARE_PACK_NAME = "cn.cgmcare.app";
+        public static final String XDRIP_PACK_NAME = "com.eveningoutpost.dexdrip";
     }
 
     /*
@@ -42,11 +60,15 @@ public class NotificationListenerExampleService extends NotificationListenerServ
         the notifications, to decide whether we should do something or not
      */
     public static final class InterceptedNotificationCode {
+        public static final int OTHER_NOTIFICATIONS_CODE = 0; // We ignore all notification with code == 0
         public static final int FACEBOOK_CODE = 1;
         public static final int WHATSAPP_CODE = 2;
         public static final int INSTAGRAM_CODE = 3;
-        public static final int OTHER_NOTIFICATIONS_CODE = 4; // We ignore all notification with code == 4
+        public static final int CGMCARE_CODE = 4;
+        public static final int XDRIP_CODE = 5;
     }
+
+    private static Map<String, String> trendMap = new HashMap<String, String>();
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -54,24 +76,113 @@ public class NotificationListenerExampleService extends NotificationListenerServ
     }
 
     @Override
-    public void onNotificationPosted(StatusBarNotification sbn){
+    public void onListenerConnected() {
+        trendMap.put("\u21c8", "DoubleUp");
+        trendMap.put("\u2191", "SingleUp");
+        trendMap.put("\u2197", "FortyFiveUp");
+        trendMap.put("\u2192", "Flat");
+        trendMap.put("\u2198", "FortyFiveDown");
+        trendMap.put("\u2193", "SingleDown");
+        trendMap.put("\u21ca", "DoubleDown");
+
+        StatusBarNotification[] activeNotifications = getActiveNotifications();
+        Log.e(TAG, "Current active notifications: " + activeNotifications.length);
+        for (int i = 0; i < activeNotifications.length; ++i) {
+            onNotificationPosted(activeNotifications[i]);
+        }
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
         int notificationCode = matchNotificationCode(sbn);
 
-        if(notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE){
-            Intent intent = new  Intent("com.github.chagall.notificationlistenerexample");
+        if (notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
+            Intent intent = new Intent("com.github.chagall.notificationlistenerexample");
             intent.putExtra("Notification Code", notificationCode);
             sendBroadcast(intent);
         }
+        Log.e(TAG, "onNotificationPosted: " + sbn.getNotification().toString());
+
+        if (notificationCode == InterceptedNotificationCode.CGMCARE_CODE &&
+                android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            Bundle extras = sbn.getNotification().extras;
+            String notifyText = extras.getString(Notification.EXTRA_TEXT);
+            Log.e(TAG, "Notification android.text: " + extras.getString(Notification.EXTRA_TEXT));
+            BgValue bgValue = processCgmCareNotify(notifyText);
+            if (bgValue != null) {
+                sendBroadcastToAAPS(bgValue);
+                sendBroadcastToxDrip(bgValue);
+            } else {
+                Log.e(TAG, "FAIL setting bg value");
+            }
+        }
     }
+
+    private void sendBroadcastToxDrip(BgValue bgValue) {
+        Bundle bundle = new Bundle();
+        bundle.putString("collection", "entries");
+        String direction = trendMap.get(bgValue.direction);
+        if (direction == null) direction = "Flat";
+        String jsonStr = String.format("[{type:sgv, direction:%s, date:%d, sgv:%f}]",
+                direction, bgValue.timestamp.getTime(), bgValue.bgValue);
+        bundle.putString("data", jsonStr);
+
+        Intent intent = new Intent("com.eveningoutpost.dexdrip.NS_EMULATOR");
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+        Log.e(TAG,"intent sent: " + intent.toString());
+    }
+
+    private void sendBroadcastToAAPS(BgValue bgValue) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean("doMgdl", true);
+        bundle.putDouble("com.eveningoutpost.dexdrip.Extras.BgEstimate", bgValue.bgValue);
+        bundle.putDouble("com.eveningoutpost.dexdrip.Extras.Raw", bgValue.bgValue);
+        bundle.putLong("com.eveningoutpost.dexdrip.Extras.Time", bgValue.timestamp.getTime());
+        String trend = trendMap.get(bgValue.direction);
+        if (trend != null) {
+            bundle.putString("com.eveningoutpost.dexdrip.Extras.BgSlopeName", trend);
+        }
+        Intent intent = new Intent("com.eveningoutpost.dexdrip.BgEstimate");
+        intent.setPackage("info.nightscout.androidaps");
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+        Log.e(TAG,"intent package: " + intent.getPackage());
+        Log.e(TAG,"intent sent: " + intent.toString());
+    }
+
+    private BgValue processCgmCareNotify(String notifyText) {
+        // Example: "时间：15:48 血糖值：9.0 ↓-0.9 达标"
+        Pattern p = Pattern.compile(".*时间：([0-9:\\.]+) 血糖值：([0-9\\.]+) ([^+-0-9]+)([+\\-0-9\\.]+)");
+        Matcher m = p.matcher(notifyText);
+        if (!m.find()) {
+            Log.e(TAG, "TEXT NOT EXTRACTED: " + notifyText);
+            return null;
+        }
+        Log.e(TAG, "TEXT EXTRACTED: " + m.group(0));
+        Log.e(TAG, "TEXT EXTRACTED: " + m.group(1));
+        Log.e(TAG, "TEXT EXTRACTED: " + m.group(2));
+        Log.e(TAG, "TEXT EXTRACTED: " + m.group(3));
+        Log.e(TAG, "TEXT EXTRACTED: " + m.group(4));
+
+        BgValue bgValue = new BgValue();
+        bgValue.setBgValue(m.group(2), "mmol");
+        bgValue.setTimestamp(m.group(1));
+        bgValue.setDirection(m.group(3));
+        bgValue.setDelta(m.group(4));
+        Log.e(TAG, "bgValue: " + bgValue.toString());
+        return bgValue;
+    }
+
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn){
         int notificationCode = matchNotificationCode(sbn);
 
-        if(notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
-
+        if (notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
             StatusBarNotification[] activeNotifications = this.getActiveNotifications();
-
             if(activeNotifications != null && activeNotifications.length > 0) {
                 for (int i = 0; i < activeNotifications.length; i++) {
                     if (notificationCode == matchNotificationCode(activeNotifications[i])) {
@@ -88,6 +199,8 @@ public class NotificationListenerExampleService extends NotificationListenerServ
     private int matchNotificationCode(StatusBarNotification sbn) {
         String packageName = sbn.getPackageName();
 
+        Log.e(TAG, "matchNotificationCode: " + packageName);
+
         if(packageName.equals(ApplicationPackageNames.FACEBOOK_PACK_NAME)
                 || packageName.equals(ApplicationPackageNames.FACEBOOK_MESSENGER_PACK_NAME)){
             return(InterceptedNotificationCode.FACEBOOK_CODE);
@@ -97,6 +210,12 @@ public class NotificationListenerExampleService extends NotificationListenerServ
         }
         else if(packageName.equals(ApplicationPackageNames.WHATSAPP_PACK_NAME)){
             return(InterceptedNotificationCode.WHATSAPP_CODE);
+        }
+        else if(packageName.equals(ApplicationPackageNames.CGMCARE_PACK_NAME)){
+            return(InterceptedNotificationCode.CGMCARE_CODE);
+        }
+        else if(packageName.equals(ApplicationPackageNames.XDRIP_PACK_NAME)){
+            return(InterceptedNotificationCode.XDRIP_CODE);
         }
         else{
             return(InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE);
